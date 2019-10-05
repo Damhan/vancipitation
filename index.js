a2efd15d7f0b7ae01cb0bab30aa646a2c16744fc
Damhan Richardson <damhanrichardson@gmail.com>
2019-10-06 00:11:49 +0100

    Created base node structure.

---
+++ index.js
@@ -0,0 +1,18 @@
+const Twit = require('twit');
+
+
+/*
+Twitter component commented out awaiting Dev Account approval.
+*/
+// const T = new Twit({
+// //PUT TWITTER DEV DEETS IN HERE
+// });
+
+// T.post(
+//     'statuses/update',
+//     {status: 'This is a weather update'},
+//     (err,data,response) => {
+//         console.log(err,data,response);
+//     }
+// );
+
