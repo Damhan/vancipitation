822cf28b5c5b649a7eb44f10ef8fb2d659a84ead
Damhan Richardson <damhanrichardson@gmail.com>
2019-10-07 21:08:13 +0100

    Added repetition to check every few hours.

--- index.js
+++ index.js
@@ -2,36 +2,50 @@ const Twit = require('twit');
 const axios = require('axios');
 const keys = require('./keys.json');
 
-/*
-Twitter component commented out awaiting Dev Account approval.
-*/
-// const T = new Twit({
-// //PUT TWITTER DEV DEETS IN HERE
-// });
+
+const T = new Twit({
+//PUT TWITTER DEV DEETS IN HERE
+    consumer_key:         keys.consumer_key,
+    consumer_secret:      keys.consumer_secret,
+    access_token:         keys.access_token,
+    access_token_secret:  keys.access_token_secret,
+    timeout_ms:           60 * 1000,
+});
 
 const rain = [500,501,502,503,504,511,520,521,522,531];
 const drizzle = [300,301,302,310,311,312,313,314,321];
 
-axios.get('http://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=' + keys.weatherKey).then(
-    response=> {
-
-        const weather = response.data.weather[0];
-        const weatherId = weather.id;
-        const weatherName = weather.main;
-        console.log(weatherId);
-        console.log(weatherName);
-
-        if (rain.includes(weatherId) || drizzle.includes(weatherId)) {
-            console.log("It's raining in Vancouver! ... again.");
-            // T.post(
-        //     'statuses/update',
-        //     {status: 'It's raining in Vancouver! ... again.},
-        //     (err,data,response) => {
-        //         console.log(err,data,response);
-        //     }
-        // );
-        }
-    })
-    .catch(error => {
-        console.log(error);
-    })
+setInterval(() => {
+    return axios.get('http://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=' + keys.weatherKey).then(
+        response=> {
+    
+            const weather = response.data.weather[0];
+            const weatherId = weather.id;
+            const weatherName = weather.main;
+            console.log(weatherId);
+            console.log(weatherName);
+    
+            if (rain.includes(weatherId) || drizzle.includes(weatherId)) {
+                console.log("It's raining in Vancouver! ... again.");
+                T.get('statuses/user_timeline', (err,data,response) => {
+                    var reg = /(\[.\])/
+                    const num = reg.exec(data[0].text);
+                    console.log(num[0][1]);
+                    var count = num[0][1];
+    
+                    T.post(
+                        'statuses/update',
+                        {status: "It's raining in Vancouver! ... again.   Rain-Count: [" + (parseInt(count)+1) + "] "},
+                        (err,data,response) => {
+                            console.log(err,data,response);
+                        }
+                    );
+    
+                });
+    
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}, 21600000);
